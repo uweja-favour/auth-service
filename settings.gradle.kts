14ca5816787e2550720a6193c4a158845fd0aa64
Favour Uweja <uwejafavour@gmail.com>
2026-05-11 15:09:19 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "auth-service"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://repo.maven.apache.org/maven2") }
+    }
+}
+
+include("contracts")
+include("platform")
+
+
